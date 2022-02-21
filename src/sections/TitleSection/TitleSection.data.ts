f81f70531082b109dc6993e006c462b75468b635
johnyworld <johnyworld@naver.com>
2022-02-21 17:55:15 +0900

    Add velog link to Title Section

--- src/sections/TitleSection/TitleSection.data.ts
+++ src/sections/TitleSection/TitleSection.data.ts
@@ -17,6 +17,7 @@ export const introduce = [
 export const contact = [
   { title: 'Github', desc: 'https://github.com/Johnyworld', href: 'https://github.com/Johnyworld' },
   { title: 'Resume', desc: 'https://johnyworld.github.io/resume', href: 'https://johnyworld.github.io/resume' },
+  { title: 'Blog', desc: 'https://velog.io/@johnyworld', href: 'https://velog.io/@johnyworld' },
   { title: 'Mobile', desc: '010-4806-3340' },
   { title: 'Email', desc: 'johnyworld@naver.com' },
 ] as Description[];
