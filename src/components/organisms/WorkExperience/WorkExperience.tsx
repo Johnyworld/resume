3cd2c975cbaa7f674405c9cff7968b34a2fcd08e
johnyworld <johnyworld@naver.com>
2022-02-23 01:16:12 +0900

    Update company name style for mobile device

--- src/components/organisms/WorkExperience/WorkExperience.tsx
+++ src/components/organisms/WorkExperience/WorkExperience.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { WorkExp } from 'types';
 import DescGrid from '../../wrappers/DescGrid';
 import Projects from '../Projects';
+import './WorkExperience.scss';
 
 export interface WorkExperienceProps {
   workExp: WorkExp;
@@ -11,8 +12,8 @@ const WorkExperience: React.FC<WorkExperienceProps> = ({ workExp }) => {
   const { company, position, from, to, projects } = workExp;
   return (
     <DescGrid key={company}>
-      <div>
-        <h3>{company}</h3>
+      <div className='work-experience'>
+        <h3 className='work-experience__company-name'>{company}</h3>
         <p>{position}</p>
         <p>
           {from} - {to}
