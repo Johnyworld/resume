a8af218de3057b90182d88d52ea85e57e33d9491
johnyworld <johnyworld@naver.com>
2023-03-30 00:14:22 +0900

    MarkdownSection 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import OtherExperienceSection from './sections/OtherExperienceSection';
-import SideProjectsSection from './sections/SideProjectsSection';
-import SkillSection from './sections/SkillsSection';
-import TitleSection from './sections/TitleSection';
-import ToyProjectsSection from './sections/ToyProjectsSection';
-import WorkExperienceSection from './sections/WorkExperienceSection';
-import ModalContextProvider from './utils/modal/modal.provider';
-import ModalRender from './utils/modal/modal.render';
+import MarkdownSection from "./sections/MarkdownSection";
+import SkillSection from "./sections/SkillsSection";
+import TitleSection from "./sections/TitleSection";
+import WorkExperienceSection from "./sections/WorkExperienceSection";
+import ModalContextProvider from "./utils/modal/modal.provider";
+import ModalRender from "./utils/modal/modal.render";
+import { sideProjects } from "./data/SideProjectsSection.data";
+import { toyProjects } from "./data/ToyProjectsSection.data";
+import { otherExperience } from "./data/OtherExperienceSection.data";
 
 function App() {
   return (
     <ModalContextProvider>
-      <div className='App'>
+      <div className="App">
         <TitleSection />
         <SkillSection />
         <WorkExperienceSection />
-        <SideProjectsSection />
-        <ToyProjectsSection />
-        <OtherExperienceSection />
+        <MarkdownSection title="Side Projects." markdown={sideProjects} />
+        <MarkdownSection title="Toy Projects." markdown={toyProjects} />
+        <MarkdownSection
+          title="Other Experience | Education."
+          markdown={otherExperience}
+        />
       </div>
       <ModalRender />
     </ModalContextProvider>
