44bb5713b0463ed79f78bbc1868887eb966ff228
johnyworld <johnyworld@naver.com>
2023-03-30 01:01:33 +0900

    필요 없는 파일들과 타입 삭제

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,25 +1,9 @@
 declare module "types" {
-  interface Description {
-    title: string;
-    desc: string;
-    href?: string;
-  }
-
-  interface Introduce {
-    title?: string;
-    desc: string;
-  }
-
   interface WorkExp {
     company: string;
     position: string;
     from: string;
     to: string;
     content: string;
-  }
-
-  interface ImageType {
-    alt: string;
-    src: string;
   }
 }
